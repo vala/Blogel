11027f21ea2a9bddd7bf0e2170b8ac20126f8d36
vala <gonjo@free.fr>
2012-02-09 14:55:09 +0100

    we prefer working generators ...

--- lib/generators/blogel/install/install_generator.rb
+++ lib/generators/blogel/install/install_generator.rb
@@ -1,45 +1,49 @@
+require File.expand_path('../utils', __FILE__)
+
 module Blogel
   class InstallGenerator < Rails::Generators::Base
+    include Generators::Utils::InstanceMethods
     source_root File.expand_path('../templates', __FILE__)
     desc "Blogel install generator"
     
     def welcome
-      say "Proceding to Blogel's installation", :blue
+      do_say "Proceding to Blogel's installation", :green
     end
     
     # The devise check code comes from RailsAdmin install generator
     def rails_admin_check
-      say "Checking if Rails Admin is present ..."
+      do_say "Checking if Rails Admin is present ..."
 
       unless defined?(RailsAdmin)
-        say "Adding Rails Admin gem to your Gemfile:"
+        do_say "Adding Rails Admin gem to your Gemfile:"
         append_file "Gemfile", "\n", :force => true
         gem 'rails_admin', :git => 'https://github.com/sferik/rails_admin.git'
       else
-        say "Rails Admin already setup, continuing ..."
+        do_say "Rails Admin already setup, continuing ..."
       end
       
       unless File.exists?(Rails.root.join("config/initializers/rails_admin.rb"))
-        say "Installing Rails Admin ..."
+        do_say "Installing Rails Admin ..."
         generate "rails_admin:install"
       else
-        say "Rails Admin installed yet, continuing ..."
+        do_say "Rails Admin installed yet, continuing ..."
       end
     end
     
     def copy_initializer_file
-      say "Installing initializer template"
+      do_say "Installing initializer template"
       copy_file "initializer.rb", "config/initializers/blogel.rb"
     end
     
     def copy_migrations
-      say "Installing migrations, don't forget to `rake db:migrate`"
-      rake "bogel:install:migrations"
+      do_say "Installing migrations, don't forget to `rake db:migrate`"
+      rake "blogel:install:migrations"
     end
     
     def mount_engine
-      mount_path = ask "Where would you like to mount Blogel engine ? default : /blog"
-      route "mount Blogel::Engine => '#{mount_path}', :as => 'blogel'"
+      mount_path = ask("Where would you like to mount Blogel engine ? [/blog]").presence || '/blog'
+      gsub_file "config/routes.rb", /mount Blogel::Engine => \'\/?.*\', :as => \'blogel\'/, ''
+      route "mount Blogel::Engine => '#{mount_path.match(/^\//) ? mount_path : "/#{mount_path}"}', :as => 'blogel'"
     end
   end
 end
---
+++ lib/generators/blogel/install/utils.rb
@@ -0,0 +1,13 @@
+module Blogel
+  module Generators
+    module Utils
+      module InstanceMethods
+        
+        def do_say str, color = :green
+          say "[ Blogel ] #{str}", color
+        end
+        
+      end
+    end
+  end
+end
