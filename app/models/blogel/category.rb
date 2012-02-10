6f049ba3d309408bff6d63e60c6e5f48f80e55e9
vala <gonjo@free.fr>
2012-02-10 16:33:36 +0100

    self join in categories works ok

--- app/models/blogel/category.rb
+++ app/models/blogel/category.rb
@@ -3,12 +3,14 @@ module Blogel
     include Blogel::ModelInstanceHelperMethods
     
     self.table_name = 'blogel_categories'
+    
     # Category's posts
-    has_and_belongs_to_many :posts, :join_table => "blogel_categories_posts", :foreign_key => "category_id"
+    has_and_belongs_to_many :posts, :join_table => "blogel_categories_posts", :foreign_key => 'category_id'
+    
     # Category hierarchy
-    has_many :children, :class_name => 'Category'
-    belongs_to :parent, :class_name => 'Category', :foreign_key => 'parent_id'
-
+    belongs_to :parent_category, :class_name => 'Category', :foreign_key => 'parent_id' #, :inverse_of => 'children_categories'
+    has_many :children_categories, :class_name => 'Category', :foreign_key => 'parent_id' #, :inverse_of => 'parent_category'
+        
     # Retrieve categories ordered by display index
     scope :ordered, order('display_order_index ASC')
     
