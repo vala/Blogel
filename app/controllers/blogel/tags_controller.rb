5c7471d681955c0f0361ba0263b8de70b05527e3
vala <gonjo@free.fr>
2012-03-23 16:30:12 +0100

    tweaks

--- app/controllers/blogel/tags_controller.rb
+++ app/controllers/blogel/tags_controller.rb
@@ -3,7 +3,7 @@ module Blogel
     layout Blogel.blog_layout
     
     def index
-      tags = Tag.all
+      tags = Tag.order('name ASC')
       @tags = Hash[('a'..'z').map {|letter| [letter, tags.reject {|tag| !tag.name.match(/^#{letter}/i)}]}]
     end
   end
