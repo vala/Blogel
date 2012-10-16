bcc8897d54c78286823278f08e0681d1ff9a4029
vala <gonjo@free.fr>
2012-10-16 19:06:20 +0200

    delete helpers requiring since they do not exist anymore

--- lib/blogel.rb
+++ lib/blogel.rb
@@ -1,37 +1,36 @@
 require "blogel/engine"
-require "blogel/helpers"
 
 module Blogel
   mattr_accessor :app_root
-  
+
   # Default user configuration
   mattr_accessor :user_model_name
   @@user_model_name = 'User'
-  
+
   # Default current user method
   mattr_accessor :current_user_method
   @@current_user_method = 'current_user'
-  
+
   # Add more fields to posts
   mattr_accessor :additional_post_fields
   @@additional_post_fields = []
-  
+
   # Fields to search with search action in posts controller
   mattr_accessor :post_search_fields
   @@post_search_fields = [:title, :content]
-  
+
   # Add more styles to post images
   mattr_accessor :additional_post_image_styles
   @@additional_post_image_styles = {}
-  
+
   # Layout for the posts controller
   mattr_accessor :blog_layout
   @@blog_layout = 'application'
-  
+
   # Number of posts shown per page ... yes !
   mattr_accessor :posts_per_page
   @@posts_per_page = 10
-  
+
   # Config block for the initializer
   def self.config
     yield self
